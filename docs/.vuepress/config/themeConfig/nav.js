a19d8f286e691cf696ea190f3c88876b6030d872
cl1997 <leiloloaa@gmail.com>
2021-03-05 16:08:56 +0800

    增加面试题

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -55,7 +55,7 @@ module.exports = [
         text: 'Interview',
         link: '/technology/',
         items: [
-            { text: '面试', link: '/pages/aea6571b7a8bae86/' },
+            { text: '面试', link: '/pages/ef8039/' },
         ],
     }, {
         text: 'more',
