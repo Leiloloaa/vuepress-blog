cf7da5618b452f4b1cca14eb53c6e2a064be1e8f
cl1997 <leiloloaa@gmail.com>
2021-03-09 16:52:58 +0800

    20210309新增面试文章

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -11,7 +11,7 @@ module.exports = {
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
-    editLinks: true, // 启用编辑
+    editLinks: false, // 启用编辑
     editLinkText: '编辑',
 
     //*** 以下配置是Vdoing主题改动和新增的配置 ***//
