74869bde9b8303ed002b8719f959e6a278e7c8cb
cl1997 <leiloloaa@gmail.com>
2021-06-05 16:03:19 +0800

    20210605新增文章

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,35 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.yourwebsite.com' > CNAME
+# deploy to github pages
+echo 'b.xugaoyi.com' > CNAME
 
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:xugaoyi/vuepress-theme-vdoing.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@github.com/xugaoyi/vuepress-theme-vdoing.git
+  git config --global user.name "xugaoyi"
+  git config --global user.email "894072666@qq.com"
+fi
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
+git push -f $githubUrl master:gh-pages # 推送到github gh-pages分支
 
-# 如果你想要部署到 https://USERNAME.github.io
-git push -f git@github.com:Leiloloaa/Leiloloaa.github.io.git master
+# deploy to coding pages
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
 
-# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
-# git push -f git@github.com:USERNAME/<REPO>.git master:gh-pages
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:xgy/xgy.git
+# else
+#   codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/xgy.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 cd -
+rm -rf docs/.vuepress/dist
