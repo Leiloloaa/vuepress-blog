d15689d30b1a2c6d56ca79b9471ed5fe848bbbf3
cl1997 <leiloloaa@gmail.com>
2021-06-28 16:59:35 +0800

    修改图片路径

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -6,8 +6,8 @@ module.exports = [
         link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
         items: [
             // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
-            { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-            { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
+            // { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
+            // { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
             // { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
             { text: '《JavaScript》笔记', link: '/note/javascript/' },
             // { text: '《JavaScript高级程序设计》笔记', link: '/note/js/' },
@@ -31,42 +31,44 @@ module.exports = [
             //     ],
             // },
         ],
-    }, {
-        text: 'Vue',
-        link: '/ui/',
-        items: [
-            { text: '《Vue》笔记', link: '/note/vue/' },
-        ],
-    }, {
-        text: 'Node.js',
-        link: '/ui/',
-        items: [
-            { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-        ],
-    }, {
-        text: 'Technology',
-        link: '/technology/',
-        items: [
-            { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-            // { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-            // { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-        ],
-    }, {
-        text: 'Interview',
-        link: '/technology/',
-        items: [
-            { text: '面试知识汇总', link: '/pages/ef8039/' },
-        ],
-    }, {
-        text: 'more',
-        link: '/more/',
-        items: [
-            { text: '学习', link: '/pages/f2a556/' },
-            { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-            { text: '实用技巧', link: '/pages/baaa02/' },
-            // { text: '友情链接', link: '/friends/' },
-        ],
     },
+    // {
+    //     text: 'Vue',
+    //     link: '/ui/',
+    //     items: [
+    //         { text: '《Vue》笔记', link: '/note/vue/' },
+    //     ],
+    // }, {
+    //     text: 'Node.js',
+    //     link: '/ui/',
+    //     items: [
+    //         { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
+    //     ],
+    // }, {
+    //     text: 'Technology',
+    //     link: '/technology/',
+    //     items: [
+    //         { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
+    //         // { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
+    //         // { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
+    //     ],
+    // },
+    // }, {
+    //     text: 'Interview',
+    //     link: '/technology/',
+    //     items: [
+    //         { text: '面试知识汇总', link: '/pages/ef8039/' },
+    //     ],
+    // }, {
+    //     text: 'more',
+    //     link: '/more/',
+    //     items: [
+    //         { text: '学习', link: '/pages/f2a556/' },
+    //         { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
+    //         { text: '实用技巧', link: '/pages/baaa02/' },
+    //         // { text: '友情链接', link: '/friends/' },
+    //     ],
+    // },
     // { text: '关于', link: '/about/' },
     // {
     //     text: '收藏',
