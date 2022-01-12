a8bb37e6ed3755b474ac3d97d31962376b0244e0
cl1997 <Leiloloaa@gmail.com>
2022-01-12 17:39:01 +0800

    增加文章

--- docs/02.面试/55.arithmetic/04.栈与队列/02.main.js
+++ docs/02.面试/55.arithmetic/04.栈与队列/02.main.js
@@ -1,4 +1,7 @@
 const isValid = function(s) {
+    // 分析 有几种情况
+    // 1、数量不对 左右多或少
+    // 2、数量对 但是 方向不对
     if ((s.length & 1) === 1) return false;
     let stack = [];
     for (let i = 0; i < s.length; i++) {
