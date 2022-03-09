528cdc5e4f23011361a11a901c1587319b6edb5b
cl1997 <Leiloloaa@gmail.com>
2022-03-09 22:26:21 +0800

    更新文章

--- docs/02.面试/00.LeetCode/01.字符串类/00.main.js
+++ docs/02.面试/00.LeetCode/01.字符串类/00.main.js
@@ -34,17 +34,66 @@
 // console.log(isPalindrome('0P'));
 
 var singleNumber = function(nums) {
-  for (let item of nums) {
-    console.log(item);
-    // console.log(nums.indexOf(nums[i]), nums.lastIndexOf(nums[i]));
-    // if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) {
-    //   return nums[i];
-    // }
-  }
+    for (let item of nums) {
+        console.log(item);
+        // console.log(nums.indexOf(nums[i]), nums.lastIndexOf(nums[i]));
+        // if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) {
+        //   return nums[i];
+        // }
+    }
 };
 
-singleNumber([4, 1, 2, 1, 2])
+// singleNumber([4, 1, 2, 1, 2]);
 // console.log(singleNumber([4, 1, 2, 1, 2]));
 
 // console.log([4, 1, 2, 1, 2].indexOf(4));
 // console.log([4, 1, 2, 1, 2].lastIndexOf(4));
+
+function getSum(num) {
+    let sum = 1
+    while (num) {
+        sum = num * sum
+        num--
+    }
+    return sum
+}
+
+function getSum2(num) {
+    if (num <= 0) {
+        return 1
+    } else {
+        return num * getSum2(num - 1)
+    }
+}
+
+// console.log(getSum2(3));
+
+// 扁平化数组 [1,[2,3,[4]]]
+function changeArr(arr) {
+    let result = []
+    for (const item of arr) {
+        if (Array.isArray(item)) {
+            result = result.concat(changeArr(item))
+        } else {
+            result.push(item)
+        }
+    }
+    return result
+}
+
+function changeArr2(arr) {
+    // return arr.toString().split('');
+    return arr.flat(2)
+}
+
+console.log(changeArr2([1, [2, 3, [4]]]));
+// console.log([1, [2, 3, [4]]].toString());
+
+// set 集合的时间复杂度
+// 普通数组的的查找都是 o(n)
+// set 集合则是 o(1)
+
+// max 的时间复杂度
+const a = [1, 3, 5, 6]
+
+console.log(Math.max(...a));
