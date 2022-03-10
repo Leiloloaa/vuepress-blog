c11d049aae515824728e12afe31ad81f1bf45d84
cl1997 <Leiloloaa@gmail.com>
2022-03-10 17:18:32 +0800

    更新文章

--- docs/02.面试/00.LeetCode/01.字符串类/00.main.js
+++ docs/02.面试/00.LeetCode/01.字符串类/00.main.js
@@ -34,13 +34,13 @@
 // console.log(isPalindrome('0P'));
 
 var singleNumber = function(nums) {
-    for (let item of nums) {
-        console.log(item);
-        // console.log(nums.indexOf(nums[i]), nums.lastIndexOf(nums[i]));
-        // if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) {
-        //   return nums[i];
-        // }
-    }
+  for (let item of nums) {
+    console.log(item);
+    // console.log(nums.indexOf(nums[i]), nums.lastIndexOf(nums[i]));
+    // if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) {
+    //   return nums[i];
+    // }
+  }
 };
 
 // singleNumber([4, 1, 2, 1, 2]);
@@ -50,40 +50,40 @@ var singleNumber = function(nums) {
 // console.log([4, 1, 2, 1, 2].lastIndexOf(4));
 
 function getSum(num) {
-    let sum = 1
-    while (num) {
-        sum = num * sum
-        num--
-    }
-    return sum
+  let sum = 1;
+  while (num) {
+    sum = num * sum;
+    num--;
+  }
+  return sum;
 }
 
 function getSum2(num) {
-    if (num <= 0) {
-        return 1
-    } else {
-        return num * getSum2(num - 1)
-    }
+  if (num <= 0) {
+    return 1;
+  } else {
+    return num * getSum2(num - 1);
+  }
 }
 
 // console.log(getSum2(3));
 
 // 扁平化数组 [1,[2,3,[4]]]
 function changeArr(arr) {
-    let result = []
-    for (const item of arr) {
-        if (Array.isArray(item)) {
-            result = result.concat(changeArr(item))
-        } else {
-            result.push(item)
-        }
+  let result = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(changeArr(item));
+    } else {
+      result.push(item);
     }
-    return result
+  }
+  return result;
 }
 
 function changeArr2(arr) {
-    // return arr.toString().split('');
-    return arr.flat(2)
+  // return arr.toString().split('');
+  return arr.flat(2);
 }
 
 console.log(changeArr2([1, [2, 3, [4]]]));
@@ -94,6 +94,41 @@ console.log(changeArr2([1, [2, 3, [4]]]));
 // set 集合则是 o(1)
 
 // max 的时间复杂度
-const a = [1, 3, 5, 6]
+const a = [1, 3, 5, 6];
 
 console.log(Math.max(...a));
+
+// 求阶层
+function getSum(num) {
+  return num * getSum(num - 1);
+}
+
+// 扁平化数组 [1,[2,3,[4,5,6]]]
+function changeArr(arr) {
+  let result = [];
+  for (const item of arr) {
+    if (Array.isArray(item)) {
+      result = result.concat(changeArr(item));
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+}
+
+function changeArr(arr) {
+  return arr.toString().split('');
+}
+
+// 双指针 翻转字符串 abcd -> dcba
+function reverseStr(str) {
+  let l = 0,
+    r = str.length - 1;
+  let res = str.split('');
+  while (++l < --r) {
+    [res[l], res[r]] = [res[r], res[l]];
+  }
+  return res.join('');
+}
+
+console.log(reverseStr('abcd'));
